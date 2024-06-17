ed0fe41226214a3e71b0ab1a3f6e0609a92eb3c1
Gabriel Pereira <gabriel02ps@gmail.com>
2024-06-17 17:33:59 -0300

    criando chart wodget

---
+++ lib/components/chart_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ChartBar extends StatelessWidget {
+  final String? label;
+  final double? value;
+  final double? percentage;
+
+  const ChartBar(
+      {super.key, this.label, this.value = 0.0, this.percentage = 0.0});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('R\$${value!.toStringAsFixed(2)}'),
+        const SizedBox(
+          height: 5,
+        ),
+        SizedBox(
+          height: 60,
+          width: 10,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage!,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(label!),
+      ],
+    );
+  }
+}
